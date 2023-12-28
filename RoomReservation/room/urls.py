53196669d221ab94eb1e0f9ac54df696ee56b3ea
stevenrhenaldy <stevenrhenaldy@gmail.com>
2023-12-28 18:15:24 +0800

    Room queries

--- RoomReservation/room/urls.py
+++ RoomReservation/room/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("room.urls")),
+    path("", views.index, name="room_index"),
+    path("<int:id>", views.show, name="room_show"),
+    
 ]
--- RoomReservation/room/views.py
+++ RoomReservation/room/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Room
+from django.shortcuts import render, get_object_or_404
+from .models import Room
 
 # Create your views here.
+def index(request):
+    rooms = Room.objects.all()
+    return render(request, 'room/index.html',{
+        'rooms': rooms
+    })
+
+
+    
+def show(request, id):
+    room = get_object_or_404(Room, id=id)
+    return render(request, 'room/show.html', {
+        'room': room
+    })
+
