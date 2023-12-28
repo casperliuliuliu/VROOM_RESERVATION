482212ae922e25ac890d46b6f1698c008301813e
stevenrhenaldy <stevenrhenaldy@gmail.com>
2023-12-28 18:15:08 +0800

    Update admin.py

--- RoomReservation/room/admin.py
+++ RoomReservation/room/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Room, MeetingRoomEquipment
+from .models import Room, MeetingRoomFacilities
 
 # Register your models here.
 admin.site.register(Room)
-admin.site.register(MeetingRoomEquipment)
+admin.site.register(MeetingRoomFacilities)
