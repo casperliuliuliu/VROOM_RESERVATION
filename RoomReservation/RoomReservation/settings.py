d4ff3371ab7f5864b5ddccbd937494c335bc05ad
stevenrhenaldy <stevenrhenaldy@gmail.com>
2023-12-31 18:20:48 +0800

    Show Room's image

--- RoomReservation/RoomReservation/settings.py
+++ RoomReservation/RoomReservation/settings.py
@@ -131,10 +131,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIAFILES_DIRS = [
+    BASE_DIR / "media",
+    # "/var/www/media/",
+]
 
 
 # Default primary key field type
